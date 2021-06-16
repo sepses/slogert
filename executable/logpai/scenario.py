42e377768b657f03e1bea979a37d35e9bdbc4b7f
Fajar <fajar.ekaputra@tuwien.ac.at>
2021-06-16 16:46:14 +0200

    updates on logpai patterns and python

--- executable/logpai/scenario.py
+++ executable/logpai/scenario.py
@@ -6,28 +6,31 @@ from logparser import Drain
 
 # Regular expression list for optional preprocessing (default: [])
 regex      = [
-    # r'(?<=[^A-Za-z0-9])(\-?\+?\d+)(?=[^A-Za-z0-9])|[0-9]+$', # Numbers
-
+    r'<?([a-zA-Z0-9.!#$%&\'*+\/?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>?' , # email
+    r'(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)', # http/s
+    r'(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)', # IP
     r'(?<=()[\/:|=\<])([A-Za-z0-9._\/-]+(?=\>))', # \w+[/:|=]\w+
     r'(?<=()[\/:|=\(])([A-Za-z0-9._\/-]+(?=\)))', # \w+[/:|=]\w+
-
     r'(?<=\[)\w+?(?=\])', # inside of a square bracket (e.g., username)
     r'(?<=\<)\w+?(?=\>)', # inside of a square bracket (e.g., username)
     r'(?<=\()\w+?(?=\))', # inside of a square bracket (e.g., username)
-
     r'(?<=()[\/:|=])([A-Za-z0-9._\/-]+)', # \w+[/:|=]\w+
-
     r'(?<=()for user )([A-Za-z0-9._\/-]+)', # username specific for auth
+    r'(?<=()login for )([A-Za-z0-9._\/-]+)', # username on login
+    r'(?<=()LOGIN for )([A-Za-z0-9._\/-]+)', # username on login
+    r'(?<=()sent to )([A-Za-z0-9._\/-]+)', # username on messages
+    r'(?<=()from )([A-Za-z0-9._\/-]+)', # username / ip
+    r'(?<=()pid )([0-9]+)', # pid
+    r'(?<=()pid=)([0-9]+)', # pid
     r'blk_(|-)[0-9]+' , # block id
-    r'(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)', # http/s
-    r'(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)', # IP
+    r'(?<=[^A-Za-z0-9])(\-?\+?\d+)(?=[^A-Za-z0-9])|[0-9]+$', # Numbers
 ]
 st         = 0.5  # Similarity threshold
 depth      = 4  # Depth of all leaf nodes
 
-input_dir  = 'output/mail.log/1-init' # The input directory of log file
-output_dir = 'output/mail.log/2-logpai' # The output directory of parsing results
-log_file   = 'mail.log.0' # The input log file name
+input_dir  = 'output/syslog/1-init' # The input directory of log file
+output_dir = 'output/syslog/2-logpai' # The output directory of parsing results
+log_file   = 'syslog.17' # The input log file name
 log_format = '<Device> <Month> <Date> <Time> <Type> <Component>: <Content>' # Syslog log format
 
 if not os.path.exists(output_dir):
